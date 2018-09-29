18340ebe17bcbf1fbe0ae53dafea237e1e539172
zgreening <zachary.greening@comcast.net>
2018-09-29 10:43:10 -0400

    Rewrote class ItemType to use full name as enum and have a code constant

--- src/ProductionLine/ItemType.java
+++ src/ProductionLine/ItemType.java
@@ -3,7 +3,7 @@
 // By:   Greening, Zachary
 // Date: September 28, 2018
 // Description: An enumerator for the class of item (ie a type of audio for
-//      music)
+//      music).
 //
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -11,36 +11,14 @@ package ProductionLine;
 
 
 public enum ItemType {
-    NA,     //Not Assigned
-    AU,     //Audio
-    VI,     //Visual
-    AM,     //AudioMobile
-    VM;     //VisualMobile
+    AUDIO("AU"),
+    VISUAL("VI"),
+    AUDIO_MOBILE("AM"),
+    VISUAL_MOBILE("VM");
 
+    String code;                    //Letter code indicating the item type
 
-    /**
-     * Overrides toString to return the full name of the Item code
-     * rather than the two letter code.
-     *
-     * @return full name for the two letter Item Code
-     */
-    @Override
-    public String toString() {
-        String returnValue;
-
-        if (ordinal() == NA.ordinal())
-            returnValue = "Not Assigned";
-        else if (ordinal() == AU.ordinal())
-            returnValue = "Audio";
-        else if (ordinal() == VI.ordinal())
-            returnValue = "Visual";
-        else if (ordinal() == AM.ordinal())
-            returnValue = "Audio Mobile";
-        else if (ordinal() == VM.ordinal())
-            returnValue = "Visual Mobile";
-        else
-            returnValue = "Unrecognized Type";
-
-        return returnValue;
+    ItemType(String code) {
+        this.code = code;
     }
 }
